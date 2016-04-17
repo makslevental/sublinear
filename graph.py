ddcb3df965cc05e7cb2ad26113245fe7e8117965
maksim <maksim.levental@gmail.com>
2016-04-16 23:01:08 -0400

    basic testing

--- graph.py
+++ graph.py
@@ -1,65 +1,50 @@
-import numpy as np
+from itertools import product
+import random
 from collections import deque
-from functools import partial
-
-def universal_sink(mat):
-
-    shp = np.shape(mat)
-    i = 0
-    j = 1
-    while i < shp[0] and j < shp[1]:
-        if mat[i,j] == 0:
-            j += 1
-        elif mat[i,j] == 1:
-            i += 1
-
-    if i == 0:
-        check = True
-        for k in range(0,shp[0]):
-            if mat[k,0] == 0:
-                check = False
-    elif i == shp[0]:
-        check = True
-        for k in range(0,i):
-            if mat[k,j] == 0:
-                check = False
-    else:
-        check = False
-
-    return check
-
 
 class Vertex():
-    def __init__(self,name:str=None,value=None,children:dict=None):
+    def __init__(self,name:str,value=None,children:list=None):
         if name is not None:
             self.name = name
         if value is not None:
             self.value = value
+        else:
+            self.value = None
+
         if children is not None:
             self.children = children
         else:
-            self.children = set()
+            self.children = []
+            # maybe cache misses will be bad?
+            # Q is long long 8 bytes
+            # self.children = array('Q')
 
         self.visited = 0
         self.distance = 0
         self.parent = None
 
     def __setattr__(self, item, value):
-        setattr(self,item,value)
+        self.__dict__[item] = value
         return self
+
+    def __str__(self):
+        if self.value is not None:
+            return str(self.value)
+        else:
+            return self.name
 
     def deg(self):
         return len(self.children)
 
 class Graph():
-    def __init__(self, vertices, edges):
-        self.vertices = {}
-        for v in vertices:
-            self.vertices[v] = Vertex(name=str(v))
+    def __init__(self, vertices:list, edges:[tuple]):
+        self.vertices = len(vertices)*[None]
+        for i,v in enumerate(vertices):
+            self.vertices[i] = Vertex(name=str(v))
         # edges are directed. undirected graphs will be represented by double edges
         self.edges = edges
         for e in self.edges:
-            self.vertices[e[1]].children.add(self.vertices[e[2]])
+            self.vertices[e[0]].children.append(self.vertices[e[1]])
 
     def bfs(self,root=None):
         if root is not None:
@@ -68,13 +53,19 @@ class Graph():
             ptr = self.vertices[1]
 
         ptr.distance = 0
-        ptr.parent = None
         q = deque([ptr])
         while len(q) > 0:
             ptr = q.popleft()
-            q.append(setattr(setattr(v,'parent',ptr),'distance',ptr.distance+1) for v in ptr.children if v.visited != 1)
+            print(ptr)
+
+            q.extend(list(v.__setattr__('parent',ptr).__setattr__('distance',ptr.distance+1) for v in ptr.children if v.visited != 1))
             ptr.visited = 1
 
     def avg_degree(self):
         pass
 
+if __name__ == '__main__':
+    vertices = list(range(0,100))
+    edges = random.sample(list(product(vertices,vertices)),500)
+    g = Graph(vertices,edges)
+    g.bfs()
